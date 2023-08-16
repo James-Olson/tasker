7137bb011dd56aa46c98906d4232d129008c63de
Jim Olson <jim.olson.456@gmail.com>
2023-08-15 22:30:53 -0500

    moving to disply pgs

--- src/main/java/com/codeup/tasker/Repos/TaskBoardRepo.java
+++ src/main/java/com/codeup/tasker/Repos/TaskBoardRepo.java
@@ -3,6 +3,6 @@ package com.codeup.tasker.Repos;
 import com.codeup.tasker.Models.TaskBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskBoardRepo extends JpaRepository<TaskBoard, Long> {
-    TaskBoard findById(Long id);
-}
+//public interface TaskBoardRepo extends JpaRepository<TaskBoard, Long> {
+//    TaskBoard findById(Long id);
+//}

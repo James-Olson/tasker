1d020beae4ec88e16b14e6ebe9a2462421ba5445
Jim Olson <jim.olson.456@gmail.com>
2023-08-21 06:33:54 -0500

    working on layouts

--- src/main/java/com/codeup/tasker/Repos/TaskBoardRepo.java
+++ src/main/java/com/codeup/tasker/Repos/TaskBoardRepo.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 //public interface TaskBoardRepo extends JpaRepository<TaskBoard, Long> {
 //    TaskBoard findById(Long id);
+//
 //}
--- src/main/java/com/codeup/tasker/controllers/TaskerBoardSelectionController.java
+++ src/main/java/com/codeup/tasker/controllers/TaskerBoardSelectionController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class TaskerBoardSelection {
+public class TaskerBoardSelectionController {
     @GetMapping("taskerboardselection")
     public String TaskerBoardSelection() {
         return "TaskerBoardSelection";

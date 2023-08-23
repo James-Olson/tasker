d62fa8b5bfae4503965f2f09464c055d1dd809ae
Jim Olson <jim.olson.456@gmail.com>
2023-08-22 20:40:35 -0500

    finishing user

---
+++ src/main/java/com/codeup/tasker/controllers/SignupController.java
@@ -0,0 +1,35 @@
+package com.codeup.tasker.controllers;
+
+import com.codeup.tasker.Models.User;
+import com.codeup.tasker.Repos.UserRepo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+public class SignupController {
+
+
+    private final UserRepo usersDao;
+
+    public SignupController(UserRepo usersDao) {
+        this.usersDao = usersDao;
+    }
+
+
+    @GetMapping("signup")
+    public String Login(Model model) {
+        model.addAttribute("user", new User());
+        return "Home";
+    }
+
+    @PostMapping("signup")
+    public String Signup(@ModelAttribute User user) {
+        usersDao.save(user);
+        return "Home";
+    }
+}
+
